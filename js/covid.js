9295e6d19df0dbff163ad8423079ef372cce6b84
“JonathinK” <jonathin.kish@gmail.com>
2021-04-27 08:50:50 -0400

    Fixed mobile where wrap bar for closing element in covid modal was still showing. Also fixed the text box that was causing a weird loayout shift because of height auto

--- js/covid.js
+++ js/covid.js
@@ -9,15 +9,21 @@ const covidClose = document.getElementById('menuClose');
 //Get elements the will change on click within modal//
 const covidIframe = document.querySelector('modal_iframe');
 //Get close wrap element//
-const closeWrap = document.getElementById('closeWrap');
+const closeWrap = document.querySelector('.close_wrap');
+//Get the no scroll query//
+const covidScroll = document.querySelector('body');
 
 //Event listener Function to open menu and apply different animation styles//
 covidOpen.addEventListener('click', () => {
     covidModal.style.width = "100%";
-    scrollContainer.style.opacity = "1"
+    scrollContainer.style.opacity = "1";
     scrollContainer.style.transition = "opacity .5s ease-in .8s";
     closeWrap.style.opacity = "1";
     covidClose.style.opacity = "1";
+    closeWrap.style.width = "100%";
+    closeWrap.style.opacity = "1";
+    closeWrap.style.pointerEvents = "all";
+    covidScroll.classList.add('noScroll');
 })
 
 //Event Listener to Close the menu and remove the styles//
@@ -27,4 +33,8 @@ covidClose.addEventListener('click', () => {
     scrollContainer.style.transition = "opacity .2s ease-out";
     closeWrap.style.opacity = "0";
     covidClose.style.opacity = "0";
+    closeWrap.style.width = "0";
+    closeWrap.style.opacity = "0";
+    closeWrap.style.pointerEvents = "none"; 
+    covidScroll.classList.remove('noScroll');
 })
