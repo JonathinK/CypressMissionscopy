2845e376a63fedeb173c358745973ade23103cbe
“JonathinK” <jonathin.kish@gmail.com>
2021-05-25 14:35:43 -0400

    Made a vast amount of changes

--- js/covid.js
+++ js/covid.js
@@ -5,36 +5,32 @@ const scrollContainer = document.getElementById('scrollContainer');
 //Get element to open covid modal//
 const covidOpen = document.getElementById('modalOpen');
 //Get the element to close the modal//
-const covidClose = document.getElementById('menuClose');
+const covidClose = document.querySelector('.close_wrap');
 //Get elements the will change on click within modal//
-const covidIframe = document.querySelector('modal_iframe');
+const covidIframe = document.querySelector('.covid_iframe');
 //Get close wrap element//
-const closeWrap = document.querySelector('.close_wrap');
+const closeWrap = document.querySelector('.title_exit_wrap');
 //Get the no scroll query//
 const covidScroll = document.querySelector('body');
 
 //Event listener Function to open menu and apply different animation styles//
 covidOpen.addEventListener('click', () => {
     covidModal.style.width = "100%";
+    covidModal.style.opacity = "1";
+    covidModal.style.pointerEvents = "all";
     scrollContainer.style.opacity = "1";
-    scrollContainer.style.transition = "opacity .5s ease-in .8s";
+    scrollContainer.style.transition = "opacity .5s ease-in";
     closeWrap.style.opacity = "1";
-    covidClose.style.opacity = "1";
-    closeWrap.style.width = "100%";
-    closeWrap.style.opacity = "1";
-    closeWrap.style.pointerEvents = "all";
     covidScroll.classList.add('noScroll');
 })
-
-//Event Listener to Close the menu and remove the styles//
+//Close the modal//
 covidClose.addEventListener('click', () => {
     covidModal.style.width = "0";
+    covidModal.style.pointerEvents = "none";
     scrollContainer.style.opacity = "0";
-    scrollContainer.style.transition = "opacity .2s ease-out";
+    scrollContainer.style.transition = "opacity .1ms ease-out";
     closeWrap.style.opacity = "0";
-    covidClose.style.opacity = "0";
-    closeWrap.style.width = "0";
-    closeWrap.style.opacity = "0";
-    closeWrap.style.pointerEvents = "none"; 
     covidScroll.classList.remove('noScroll');
 })
+
+
--- js/nav.js
+++ js/nav.js
@@ -1,80 +1,94 @@
 const openNav = document.getElementById('openMenu');
-const mobileMenu = document.querySelector('.nav_wrap');
+const mobileMenu = document.querySelector('.nav_links');
 const closeNav = document.getElementById('closeMenu');
-const showLinks = document.querySelector('.nav_links');
 const covidLink = document.getElementById('modalOpen');
-const dropTrigger = document.getElementById('dropTrigger');
-const dropMenu = document.getElementById('dropDown');
-const link1 = document.getElementById('link1');
-const link2 = document.getElementById('link2');
-const line = document.querySelectorAll('.linkHover::before');
-const lineHover = document.querySelector('.linkHover:hover:before');
+const link1 = document.querySelector('.nav_links .link1');
+const link2 = document.querySelector('.nav_links .link2');
+const link3 = document.querySelector('.nav_links .link3');
+const link4 = document.querySelector('.nav_links .link4');
+const link5 = document.querySelector('.nav_links .link5');
+const link6 = document.querySelector('.nav_links .link6');
 const body = document.querySelector('body');
-//Drop Menu Open and Close//
-dropTrigger.addEventListener('click', () => {
-    if(dropMenu.classList.contains('dropOpen')){
-        dropMenu.classList.remove('dropOpen');
-        link1.classList.remove('linksShow');
-        link2.classList.remove('linksShow');
-    }else{
-        dropMenu.classList.add('dropOpen');
-        link1.classList.add('linksShow');
-        link2.classList.add('linksShow');
-    }
-});
-
 
 //Open Nav Menu//
 openNav.addEventListener('click', () => {
-   mobileMenu.classList.add("menuOpen");
-   closeNav.style.display = ("block");
-   showLinks.classList.add("showLinks");
-   body.classList.add("noScroll"); 
+   mobileMenu.classList.add("menuOpen");   
    mobileMenu.style.transition = "width .6s ease-in-out";
-   dropMenu.classList.remove('dropOpen');
-   link1.classList.remove('linksShow');
-   link2.classList.remove('linksShow');
+   closeNav.style.opacity = ("1");
+   closeNav.style.transition = ("opacity .3s ease-in .5s");
+   closeNav.style.pointerEvents = ("all");
+   body.classList.add("noScroll"); 
+   link1.classList.add("slide");
+   link2.classList.add("slide");
+   link3.classList.add("slide");
+   link4.classList.add("slide");
+   link5.classList.add("slide");
+   link6.classList.add("slide");
 });
+
 //Close Nav Menu//
 closeNav.addEventListener('click', () => {
     mobileMenu.classList.remove("menuOpen");
-    closeNav.style.display = ("none");
-    showLinks.classList.remove("showLinks");
-    body.classList.remove("noScroll");
     mobileMenu.style.transition = "width .6s ease-in-out";
-    dropMenu.classList.remove('dropOpen');
-    link1.classList.remove('linksShow');
-    link2.classList.remove('linksShow');
+    closeNav.style.opacity = ("0");
+    closeNav.style.transition = ("opacity .3s ease-out");
+    closeNav.style.pointerEvents = ("none");
+    body.classList.remove("noScroll");
+    link1.classList.remove("slide");
+    link2.classList.remove("slide");
+    link3.classList.remove("slide");
+    link4.classList.remove("slide");
+    link5.classList.remove("slide");
+    link6.classList.remove("slide");
  });
+
 //If covid link is clicked then nav menu closes//
 covidLink.addEventListener('click', () =>{
     mobileMenu.classList.remove("menuOpen");
-    showLinks.classList.remove('showLinks');
+    mobileMenu.style.transition = "width .6s ease-in-out";
+    closeNav.style.opacity = ("0");
+    closeNav.style.transition = ("opacity .3s ease-out");
+    closeNav.style.pointerEvents = ("none");
+    body.classList.remove("noScroll");
+    link1.classList.remove("slide");
+    link2.classList.remove("slide");
+    link3.classList.remove("slide");
+    link4.classList.remove("slide");
+    link5.classList.remove("slide");
+    link6.classList.remove("slide");
 });
 //Removes any styles that would cause awkward pre animations on resizing//
 window.addEventListener("resize", () => {
-    if(window.innerWidth < 800){
+    if(window.innerWidth < 768){
         mobileMenu.classList.remove("menuOpen");
-        closeNav.style.display = ("none");
-        showLinks.classList.remove("showLinks");
-        body.classList.remove("noScroll");
         mobileMenu.style.transition = "none";
-        dropMenu.classList.remove('dropOpen');
-        link1.classList.remove('linksShow');
-        link2.classList.remove('linksShow');
+        closeNav.style.opacity = ("0");
+        closeNav.style.transition = ("opacity .1ms ease-out");
+        closeNav.style.pointerEvents = ("none");
+        body.classList.remove("noScroll");
+        link1.classList.remove("slide");
+        link2.classList.remove("slide");
+        link3.classList.remove("slide");
+        link4.classList.remove("slide");
+        link5.classList.remove("slide");
+        link6.classList.remove("slide");
     }
 }); 
 
 window.addEventListener("resize", () => {
-    if(window.innerWidth > 800){
+    if(window.innerWidth > 768){
         mobileMenu.classList.remove("menuOpen");
-        closeNav.style.display = ("none");
-        showLinks.classList.remove("showLinks");
-        body.classList.remove("noScroll"); 
         mobileMenu.style.transition = "none";
-        dropMenu.classList.remove('dropOpen');
-        link1.classList.remove('linksShow');
-        link2.classList.remove('linksShow');
+        closeNav.style.opacity = ("0");
+        closeNav.style.transition = ("opacity .1ms ease-out");
+        closeNav.style.pointerEvents = ("none");
+        body.classList.remove("noScroll"); 
+        link1.classList.remove("slide");
+        link2.classList.remove("slide");
+        link3.classList.remove("slide");
+        link4.classList.remove("slide");
+        link5.classList.remove("slide");
+        link6.classList.remove("slide");
     }
 });
 
